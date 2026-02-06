1ef52ec2716be932f1b3079d60dd407cceae5d0d
SMRSCRIPT <xpxzndy8rd@privaterelay.appleid.com>
2026-02-06 15:38:26 +0300

    Refactor success alert to use SuccessNotifier class

--- Tweak.mm
+++ Tweak.mm
@@ -1,46 +1,37 @@
 #import <UIKit/UIKit.h>
 
-static void ShowSuccessAlert() {
+@interface SuccessNotifier : NSObject
++ (void)show;
+@end
+
+@implementation SuccessNotifier
++ (void)show {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIWindow *mainWindow = nil;
-        
-        // الطريقة الحديثة المتوافقة مع iOS 13 وصولاً إلى iOS 26
+        UIWindow *win = nil;
         if (@available(iOS 13.0, *)) {
-            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
-                if (scene.activationState == UISceneActivationStateForegroundActive) {
-                    for (UIWindow *window in scene.windows) {
-                        if (window.isKeyWindow) {
-                            mainWindow = window;
-                            break;
-                        }
-                    }
+            for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if ([scene isKindOfClass:[UIWindowScene class]] && scene.activationState == UISceneActivationStateForegroundActive) {
+                    win = ((UIWindowScene *)scene).windows.firstObject;
+                    break;
                 }
             }
         }
-        
-        // إذا لم نجد النافذة بالطريقة الحديثة، نحاول جلب النافذة الأولى
-        if (!mainWindow) {
-            mainWindow = [UIApplication sharedApplication].windows.firstObject;
-        }
+        if (!win) win = [UIApplication sharedApplication].windows.firstObject;
 
-        if (mainWindow && mainWindow.rootViewController) {
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"0x108 System"
+        if (win && win.rootViewController) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"0x108"
                                            message:@"\n✅ تم التفعيل بنجاح"
                                     preferredStyle:UIAlertControllerStyleAlert];
-
-            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"موافق" 
-                                                               style:UIAlertActionStyleDefault 
-                                                             handler:nil];
-            [alert addAction:okAction];
-
-            [mainWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+            [alert addAction:[UIAlertAction actionWithTitle:@"موافق" style:UIAlertActionStyleDefault handler:nil]];
+            [win.rootViewController presentViewController:alert animated:YES completion:nil];
         }
     });
 }
+@end
 
 __attribute__((constructor))
 static void init() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 10 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        ShowSuccessAlert();
+        [SuccessNotifier show];
     });
 }
