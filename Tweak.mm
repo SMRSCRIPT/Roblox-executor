4158362de15740e209e0b279370806ec4ea78440
SMRSCRIPT <xpxzndy8rd@privaterelay.appleid.com>
2026-02-06 15:35:09 +0300

    Refactor alert function to show success message

--- Tweak.mm
+++ Tweak.mm
@@ -1,42 +1,46 @@
 #import <UIKit/UIKit.h>
 
-// دالة إظهار الرسالة
-static void ShowAlert() {
-    // محاولة جلب النافذة بأكثر من طريقة لضمان التوافق
-    UIWindow *window = nil;
-    if (@available(iOS 13.0, *)) {
-        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                window = scene.windows.firstObject;
-                break;
+static void ShowSuccessAlert() {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindow *mainWindow = nil;
+        
+        // الطريقة الحديثة المتوافقة مع iOS 13 وصولاً إلى iOS 26
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    for (UIWindow *window in scene.windows) {
+                        if (window.isKeyWindow) {
+                            mainWindow = window;
+                            break;
+                        }
+                    }
+                }
             }
         }
-    }
-    
-    // إذا فشلت الطريقة الأولى، نستخدم الطريقة التقليدية
-    if (!window) {
-        window = [UIApplication sharedApplication].keyWindow;
-    }
+        
+        // إذا لم نجد النافذة بالطريقة الحديثة، نحاول جلب النافذة الأولى
+        if (!mainWindow) {
+            mainWindow = [UIApplication sharedApplication].windows.firstObject;
+        }
 
-    if (window && window.rootViewController) {
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"0x108 Menu"
-                                                                       message:@"\n✅ تم التفعيل بنجاح"
-                                                                preferredStyle:UIAlertControllerStyleAlert];
+        if (mainWindow && mainWindow.rootViewController) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"0x108 System"
+                                           message:@"\n✅ تم التفعيل بنجاح"
+                                    preferredStyle:UIAlertControllerStyleAlert];
 
-        UIAlertAction *action = [UIAlertAction actionWithTitle:@"موافق" 
-                                                         style:UIAlertActionStyleDefault 
-                                                       handler:nil];
-        [alert addAction:action];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"موافق" 
+                                                               style:UIAlertActionStyleDefault 
+                                                             handler:nil];
+            [alert addAction:okAction];
 
-        [window.rootViewController presentViewController:alert animated:YES completion:nil];
-    }
+            [mainWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+        }
+    });
 }
 
-// نقطة الانطلاق
 __attribute__((constructor))
 static void init() {
-    // انتظار 10 ثوانٍ لضمان استقرار اللعبة
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 10 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        ShowAlert();
+        ShowSuccessAlert();
     });
 }
